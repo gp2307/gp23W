df771247617bece47a4c172e53baba4a0ec1e895
seagoatW <2604378462@qq.com>
2019-09-16 20:26:15 +0800

    王玉星

--- src/main/scala/Hello.scala
+++ src/main/scala/Hello.scala
@@ -1,5 +1,13 @@
+import org.apache.spark.sql.SparkSession
+
 object Hello {
   def main(args: Array[String]): Unit = {
     println("hello word")
+    val sparkSession = SparkSession.builder().master("local[2]").appName("one").getOrCreate()
+    val df = sparkSession.read.parquet("F:\\2.Hadoop\\上课视频\\数仓项目阶段\\第七周\\release_session\\bdp_day=20190613\\15603638400003h4gka4h")
+    import sparkSession.implicits._
+    import org.apache.spark.sql.functions._
+    df.show()
+    sparkSession.stop()
   }
 }
